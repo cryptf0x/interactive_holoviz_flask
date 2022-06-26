06c1471fe997f5662be73d9883546d1aa3362fd5
Arvind <arvind@lakebrains.com>
2022-06-26 07:21:31 +0530

    improved ui

--- Input from Front End/app.py
+++ Input from Front End/app.py
@@ -4,13 +4,19 @@ from flask import Flask, request, render_template
 app = Flask(__name__, template_folder="template")
 
 @app.route('/', methods=["GET", "POST"])
-def gfg():
+def homepage():
+    return render_template("form.html")
+
+@app.route('/submit_input', methods=["GET", "POST"])
+def submit_form():
     if request.method == "POST":
-        first_name = request.form.get("fname")
-        last_name = request.form.get("lname")
-        return "Your name is " + first_name + " " + last_name
-    else:
-        return render_template("form.html")
+        said_ids = request.form.get("said_ids")
+        from_date = request.form.get("from_date")
+        to_date = request.form.get("to_date")
+    print(said_ids)
+    print(from_date)
+    print(to_date)
+    return render_template("submission.html")
 
 if __name__ == '__main__':
     app.run()
